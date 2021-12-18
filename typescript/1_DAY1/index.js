97ecf5cac8951d9290df0f19a65fc2771c3f9cbd
Seb Patin <seb@Sebs-MacBook-Air.local>
2021-12-17 21:17:51 -0500

    first commit

---
+++ typescript/1_DAY1/index.js
@@ -0,0 +1,25 @@
+"use strict";
+exports.__esModule = true;
+// let Seb = "The God" //declarations can be done without initilazation 
+// const y = "something" //always with init. and cannot be reassigned
+// console.log(Seb)
+// let sum;
+// const title = "Typescript title";
+// let isSeb: boolean = true;
+// let total: number = 0;
+// let string: string = "Seb";
+// let sentence: string = `My name is ${name}`
+var dante = false;
+var Sami = true;
+function shinobiSyntax() {
+    var Seb = true;
+    if (dante == true) {
+        return "sick";
+    }
+    else {
+        return "Sami";
+    }
+}
+
+console.log(shinobiSyntax());
+//the : represents the type of variable and the = is the initialization
---
+++ typescript/1_DAY1/index.ts
@@ -0,0 +1,34 @@
+export {}
+
+// let Seb = "The God" //declarations can be done without initilazation 
+// const y = "something" //always with init. and cannot be reassigned
+
+// console.log(Seb)
+
+// let sum;
+// const title = "Typescript title";
+
+// let isSeb: boolean = true;
+// let total: number = 0;
+// let string: string = "Seb";
+
+// let sentence: string = `My name is ${name}`
+
+let dante: boolean = true
+
+let Sami: boolean = true
+
+function shinobiSyntax() {
+    let Seb: boolean = true;
+
+    if(dante == true){
+        return Seb = false
+    }
+    else {
+        return Sami = false
+    }
+
+}
+
+shinobiSyntax()
+//the : represents the type of variable and the = is the initialization
