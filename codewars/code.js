f4b56820e5dd0a525c8feb76a3e4b0b407f57455
Seb Patin <seb@Sebs-MacBook-Air.local>
2022-04-29 12:44:09 -0400

    a few more codewars problems

---
+++ codewars/code.js
@@ -0,0 +1,54 @@
+// // Create a function which answers the question "Are you playing banjo?".
+// // If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// // The function takes a name as its only argument, and returns one of the following strings:
+
+// // name + " plays banjo" 
+// // name + " does not play banjo"
+
+
+
+
+// // function run(name) {
+// // const testChar = name.charAt(0)
+// // return testChar
+// // }
+
+// // const testRun = run("Shinobisyntax")
+
+// // console.log(testRun)
+
+
+// function areYouPlayingBanjo(name) {
+//     const findR = name.charAt(0)
+//     if (findR === "r" || findR === "R") {
+//         return name + " plays banjo"
+//     } else {
+//         return name + " doesn't play banjo"
+//     }
+// } 
+
+// console.log(areYouPlayingBanjo("Reb"))
+
+
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+// function smash (words) {
+//    return words.join(" ")
+// };
+
+// console.log(smash(["hello", "world,", "I", "am", "Seb"]))
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+    
+}
+
+console.log(greet("ShinobiSyntax"))
